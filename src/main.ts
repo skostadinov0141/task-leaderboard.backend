fd88c2774f05397fb1ad262dfd207523a73b1d0a
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-02-06 22:08:10 +0100

    added swagger added swagger cli plugin added swagger declaration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Leaderboard API')
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
