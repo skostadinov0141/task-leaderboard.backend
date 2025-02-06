23e886616d25241397703de9d1b0e48703fc3317
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-02-06 22:29:49 +0100

    added mongoose module added root connection in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:secret@localhost:27017/leaderboard?authSource=admin',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
