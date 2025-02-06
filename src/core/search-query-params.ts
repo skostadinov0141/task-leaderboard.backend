83abe7753e76982daf21c819c090e7f262cd512e
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-02-07 00:12:31 +0100

    feat: add SearchQueryParams class for handling search query parameters

---
+++ src/core/search-query-params.ts
@@ -0,0 +1,37 @@
+export class SearchQueryParams {
+  /**
+   * The page number to return.
+   * @example 1
+   */
+  page: number = 1;
+
+  /**
+   * The number of items to return per page.
+   * @example 20
+   */
+  limit: number = 20;
+
+  /**
+   * The field to sort by.
+   * @example createdAt asc
+   */
+  sort?: string;
+
+  /**
+   * The search string to filter by.
+   * @example John Doe
+   */
+  searchString?: string;
+
+  /**
+   * The fields to select.
+   * @example ['name', 'email']
+   */
+  populate?: string[];
+
+  /**
+   * The fields to select.
+   * @example *
+   */
+  select: string[] | string = '*';
+}
