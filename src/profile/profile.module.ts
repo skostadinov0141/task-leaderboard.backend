39d4606e52da44d79c36f6580f279573780854d3
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-02-06 23:13:19 +0100

    added profile mongoose module

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Profile, ProfileSchema } from './core/profile.schema';
 
 @Module({
   controllers: [ProfileController],
   providers: [ProfileService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Profile.name,
+        schema: ProfileSchema,
+      },
+    ]),
+  ],
 })
 export class ProfileModule {}
