76551c8770709edbc9dedf45de75bd840b790cfd
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-02-06 22:59:50 +0100

    added profile resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb://root:secret@localhost:27017/leaderboard?authSource=admin',
     ),
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
