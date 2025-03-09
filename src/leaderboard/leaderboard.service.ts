cd700e4ab2c8326048d88f3e75e0fa375e350060
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2025-03-09 19:51:40 +0100

    feat: enhance LeaderboardService with Mongoose models for Leaderboard, Task, and Run

--- src/leaderboard/leaderboard.service.ts
+++ src/leaderboard/leaderboard.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Leaderboard } from './schemas/leaderboard.schema';
+import { Model } from 'mongoose';
+import { Task } from './schemas/task.schema';
+import { Run } from './schemas/run.schema';
 
 @Injectable()
-export class LeaderboardService {}
+export class LeaderboardService {
+  constructor(
+    @InjectModel(Leaderboard.name)
+    private readonly leaderboardModel: Model<Leaderboard>,
+    @InjectModel(Task.name) private readonly taskModel: Model<Task>,
+    @InjectModel(Run.name)
+    private readonly runModel: Model<Run>,
+  ) {}
+}
