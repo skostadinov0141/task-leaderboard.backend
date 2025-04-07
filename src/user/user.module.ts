8b82b30288cab26f29204d314958ceb803eb5ab8
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-04-08 00:21:22 +0200

    feat: add Mongoose integration for User schema in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '../core/schemas/user.schema';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
 })
 export class UserModule {}
