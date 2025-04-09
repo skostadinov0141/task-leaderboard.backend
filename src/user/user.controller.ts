4fff0abb8bb32bd91fd09f498a20433fe66f0c73
Simeon Kostadinov <88680364+skostadinov0141@users.noreply.github.com>
2025-04-09 11:29:24 +0200

    feat: add sign-in endpoint for user authentication using Google ID token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,14 +3,27 @@ import { UserService } from './user.service';
 import { Public } from '../auth/is-public.decorator';
 import { SignUpDto } from './dtos/sign-up.dto';
 import { TokenPairDto } from '../auth/dtos/token-pair.dto';
+import { SignInDto } from './dtos/sign-in.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  /**
+   * Sign up a new user using Google ID token.
+   */
   @Post('sign-up')
   @Public()
   async signUp(@Body() payload: SignUpDto): Promise<TokenPairDto> {
     return await this.userService.signUp(payload);
+  }
+
+  /**
+   * Sign in an existing user using Google ID token.
+   */
+  @Post('sign-in')
+  @Public()
+  async signIn(@Body() payload: SignInDto): Promise<TokenPairDto> {
+    return await this.userService.signIn(payload);
   }
 }
